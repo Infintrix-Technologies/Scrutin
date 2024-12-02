18245d00c9a1248255f7dd5e66e91789689ad7f9
khawarshehzad786 <khawarshehzad856@gmail.com>
2024-12-02 17:27:44 +0500

    handle the multiple check

--- scrutin_frontend/src/pages/TestPage.tsx
+++ scrutin_frontend/src/pages/TestPage.tsx
@@ -1,6 +1,9 @@
-import { useEffect, useState } from "react";
+import {
+  //  useEffect, 
+  useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { toast, Toaster } from "react-hot-toast";
@@ -11,7 +14,7 @@ import { useNavigate, useParams } from "react-router-dom";
 const TestPage = () => {
   const { candidate_id } = useParams();
   const [triggerReload, setTriggerReload] = useState(false);
-  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+  const [selectedOption, setSelectedOption] = useState<string | string[] | null>(null);
 
   const { data, isLoading, error } = useFrappeGetCall(
     "scrutin.api.candidate_test.get_current_question",
@@ -25,23 +28,29 @@ const TestPage = () => {
   const navigate = useNavigate();
 
   const handleSubmit = async () => {
-    if (!selectedOption) {
-      toast.error("Select any option before submit.");
+    if (!selectedOption || (Array.isArray(selectedOption) && selectedOption.length === 0)) {
+      toast.error("Select an option before submitting.");
       return;
     }
-
+  
     try {
       const response = await call({
         candidate_id,
         question_id: data?.message?.test?.current_question?.name,
-        answer: selectedOption,
+        answer: Array.isArray(selectedOption)
+        // Convert elements to numbers and stringify
+          ? JSON.stringify(selectedOption.map(Number)) 
+          // Handle single answer
+          : selectedOption,
       });
-
+  
       if (response?.message?.message === "All tests are completed") {
         navigate(`/candidacy/${candidate_id}/overview`);
       } else {
-        setSelectedOption(null); // Reset selection
-        setTriggerReload((prev) => !prev); // Reload questions
+        // Reset selection
+        setSelectedOption(null); 
+        // Reload questions
+        setTriggerReload((prev) => !prev); 
         toast.success("Response submitted successfully!");
       }
     } catch (error) {
@@ -49,12 +58,14 @@ const TestPage = () => {
       toast.error("There was an issue submitting your response. Please try again.");
     }
   };
+  
+  
 
-  useEffect(() => {
-    if (data?.message?.message === "All tests are completed") {
-      navigate(`/candidacy/${candidate_id}/overview`);
-    }
-  }, [data, navigate, candidate_id]);
+  // useEffect(() => {
+  //   if (data?.message?.message === "All tests are completed") {
+  //     navigate(`/candidacy/${candidate_id}/overview`);
+  //   }
+  // }, [data, navigate, candidate_id]);
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error loading test data.</p>;
@@ -62,49 +73,78 @@ const TestPage = () => {
   const test = data?.message?.test;
   const currentQuestion = test?.current_question;
 
+  const isMultiChoice = currentQuestion?.type === "Multi Choice";
+
+  const handleMultiSelectChange = (value: string) => {
+    setSelectedOption((prev) => {
+      if (Array.isArray(prev)) {
+        return prev.includes(value) ? prev.filter((v) => v !== value) : [...prev, value];
+      }
+      return [value];
+    });
+  };
+
   return (
-    <div className="flex justify-center items-center h-auto">   
+    <div className="flex justify-center items-center h-auto">
       <div className="flex flex-col px-3">
-        <h3 className="text-lg font-semibold m-2">
-                Test Name: {test?.test?.title}
-              </h3>
-      <Card className="w-full max-w-full md:min-w-[750px] lg:min-w-[800px] mx-auto">
-        <CardContent className="space-y-6">
-          
-          <div className="flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-4 my-6">
-            <div className="flex-1 p-4">
-             
-              <div
-                className="ql-editor read-mode [&_ol]:list-decimal [&_ul]:list-disc [&_li]:mb-2 [&_li]:ml-4"
-                dangerouslySetInnerHTML={{
-                  __html: `Question: ${currentQuestion?.text}`,
-                }}
-              ></div>
-            </div>
-            <div className="flex-1 py-5 px-10 space-y-3">
-              <h3 className="text-lg font-semibold mb-2">Select Answer</h3>
-              <RadioGroup
-                className="space-y-2"
-                name={`question-${currentQuestion?.name}`}
-                onValueChange={(value) => setSelectedOption(value)}
-                value={selectedOption || ""}
-              >
-                {currentQuestion?.options?.map((option: Option) => (
-                  <div key={option.value} className="flex items-center space-x-2">
-                    <RadioGroupItem value={option.value} id={option.value} />
-                    <Label htmlFor={option.value}>{option.label}</Label>
+        <div className="block md:flex md:justify-between">
+          <h3 className="text-lg font-semibold m-2">
+            Test Name: {test?.test?.title}
+          </h3>
+          <h3 className="text-lg font-semibold m-2">
+            Type: {currentQuestion?.type}
+          </h3>
+        </div>
+        <Card className="w-full max-w-full md:min-w-[750px] lg:min-w-[800px] mx-auto">
+          <CardContent className="space-y-6">
+            <div className="flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-4 my-6">
+              <div className="flex-1 p-4">
+                <div
+                  className="ql-editor read-mode [&_ol]:list-decimal [&_ul]:list-disc [&_li]:mb-2 [&_li]:ml-4"
+                  dangerouslySetInnerHTML={{
+                    __html: `Question: ${currentQuestion?.text}`,
+                  }}
+                ></div>
+              </div>
+              <div className="flex-1 py-5 px-10 space-y-3">
+                <h3 className="text-lg font-semibold mb-2">Select Answer</h3>
+                {isMultiChoice ? (
+                  <div>
+                    {currentQuestion?.options?.map((option: Option) => (
+                      <div key={option.value} className="my-2 flex items-center space-x-2">
+                        <Checkbox
+                          id={option.value}
+                          checked={Array.isArray(selectedOption) && selectedOption.includes(option.value)}
+                          onCheckedChange={() => handleMultiSelectChange(option.value)}
+                        />
+                        <Label htmlFor={option.value}>{option.label}</Label>
+                      </div>
+                    ))}
                   </div>
-                ))}
-              </RadioGroup>
+                ) : (
+                  <RadioGroup
+                    className="space-y-2"
+                    name={`question-${currentQuestion?.name}`}
+                    onValueChange={(value) => setSelectedOption(value)}
+                    value={Array.isArray(selectedOption) ? selectedOption[0] : selectedOption || ""}
+                  >
+                    {currentQuestion?.options?.map((option: Option) => (
+                      <div key={option.value} className="flex items-center space-x-2">
+                        <RadioGroupItem value={option.value} id={option.value} />
+                        <Label htmlFor={option.value}>{option.label}</Label>
+                      </div>
+                    ))}
+                  </RadioGroup>
+                )}
+              </div>
             </div>
-          </div>
-          <div className="flex justify-end">
-            <Button onClick={handleSubmit}>Submit</Button>
-          </div>
-        </CardContent>
-      </Card>
+            <div className="flex justify-end">
+              <Button onClick={handleSubmit}>Submit</Button>
+            </div>
+          </CardContent>
+        </Card>
       </div>
-      <Toaster position="top-center" reverseOrder={false} />      
+      <Toaster position="top-center" reverseOrder={false} />
     </div>
   );
 };
